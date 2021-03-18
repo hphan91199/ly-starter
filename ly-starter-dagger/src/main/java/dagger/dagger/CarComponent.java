b13c337570f6c484514fb80f553f78e8910db81c
phanhuongly <huongly.phan@zendesk.com>
2021-03-18 17:50:35 +0800

    Resolve dependencies dagger

--- ly-starter-dagger/src/main/java/dagger/dagger/CarComponent.java
+++ ly-starter-dagger/src/main/java/dagger/dagger/CarComponent.java
@@ -1,9 +1,11 @@
-package com.ly.dagger.dagger;
+package dagger.dagger;
+
+import dagger.Component;
 
 @Component
 public interface CarComponent {
 
-    public MyCar getCar();
+    public com.ly.dagger.dagger.MyCar getCar();
 
     //Dagger see MyCar and will auto implement MyCar o = new MyCar();
 }
